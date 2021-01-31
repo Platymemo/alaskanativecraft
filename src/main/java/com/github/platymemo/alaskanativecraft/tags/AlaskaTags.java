d2413153f9361f7525a9479e86bb932ff44159be
Platymemo <edwardhazelton+github@gmail.com>
2021-01-31 01:50:16 -0900

    Removed unnecessary tags

--- src/main/java/com/github/platymemo/alaskanativecraft/tags/AlaskaTags.java
+++ src/main/java/com/github/platymemo/alaskanativecraft/tags/AlaskaTags.java
@@ -2,7 +2,6 @@ package com.github.platymemo.alaskanativecraft.tags;
 
 import com.github.platymemo.alaskanativecraft.AlaskaNativeCraft;
 import com.github.platymemo.alaskanativecraft.tags.common.CommonBlockTags;
-import com.github.platymemo.alaskanativecraft.tags.common.CommonItemTags;
 import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
@@ -11,13 +10,10 @@ import net.minecraft.util.Identifier;
 public class AlaskaTags {
     public static final Tag<Item> ULUS = TagRegistry.item(new Identifier(AlaskaNativeCraft.MOD_ID, "ulus"));
     public static final Tag<Item> HARPOONS = TagRegistry.item(new Identifier(AlaskaNativeCraft.MOD_ID, "harpoons"));
-    public static final Tag<Item> DOGSLEDS = TagRegistry.item(new Identifier(AlaskaNativeCraft.MOD_ID, "dogsleds"));
-    public static final Tag<Item> KUSPUKS = TagRegistry.item(new Identifier(AlaskaNativeCraft.MOD_ID, "kuspuks"));
     public static final Tag<Item> AKUTAQ_MEATS = TagRegistry.item(new Identifier(AlaskaNativeCraft.MOD_ID, "akutaq_meats"));
     public static final Tag<Item> AKUTAQ_BERRIES = TagRegistry.item(new Identifier(AlaskaNativeCraft.MOD_ID, "akutaq_berries"));
 
     public static void register() {
         CommonBlockTags.register();
-        CommonItemTags.register();
     }
 }
--- src/main/java/com/github/platymemo/alaskanativecraft/tags/common/CommonBlockTags.java
+++ src/main/java/com/github/platymemo/alaskanativecraft/tags/common/CommonBlockTags.java
@@ -7,7 +7,6 @@ import net.minecraft.util.Identifier;
 
 public class CommonBlockTags {
     public static final Tag<Block> LOGS_WITH_BARK = TagRegistry.block(new Identifier("c", "logs_with_bark"));
-    public static final Tag<Block> CHESTS = TagRegistry.block(new Identifier("c", "chests"));
 
     public static void register() {
     }
