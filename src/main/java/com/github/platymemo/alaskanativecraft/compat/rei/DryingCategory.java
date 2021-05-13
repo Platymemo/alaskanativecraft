33c4d1c4b859beae0da2a48fb32c407bc4d27520
Platymemo <edwardhazelton+github@gmail.com>
2021-05-12 16:12:39 -0800

    Add REI support for drying

---
+++ src/main/java/com/github/platymemo/alaskanativecraft/compat/rei/DryingCategory.java
@@ -0,0 +1,56 @@
+package com.github.platymemo.alaskanativecraft.compat.rei;
+
+import com.github.platymemo.alaskanativecraft.block.AlaskaBlocks;
+import com.google.common.collect.Lists;
+import me.shedaniel.math.Point;
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.EntryStack;
+import me.shedaniel.rei.api.RecipeCategory;
+import me.shedaniel.rei.api.widgets.Widgets;
+import me.shedaniel.rei.gui.widget.Widget;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class DryingCategory implements RecipeCategory<DryingDisplay> {
+    @Override
+    public @NotNull Identifier getIdentifier() {
+        return DryingPlugin.DRYING_PLUGIN;
+    }
+
+    @Override
+    public @NotNull EntryStack getLogo() {
+        return EntryStack.create(AlaskaBlocks.DRYING_RACK);
+    }
+
+    @Override
+    public @NotNull String getCategoryName() {
+        return I18n.translate("category.rei.alaskanativecraft.drying");
+    }
+
+    @Override
+    public @NotNull List<Widget> setupDisplay(DryingDisplay display, Rectangle bounds) {
+        Point startPoint = new Point(bounds.getCenterX() - 41, bounds.y + 10);
+        final double cookingTime = display.getDryTime();
+        DecimalFormat df = new DecimalFormat("###.##");
+        List<Widget> widgets = Lists.newArrayList();
+        widgets.add(Widgets.createRecipeBase(bounds));
+        widgets.add(Widgets.createResultSlotBackground(new Point(startPoint.x + 61, startPoint.y + 9)));
+        widgets.add(Widgets.createBurningFire(new Point(startPoint.x + 1, startPoint.y + 20)).animationDurationMS(10000));
+        widgets.add(Widgets.createLabel(new Point(bounds.x + bounds.width - 5, bounds.y + 5),
+                new TranslatableText("category.rei.campfire.time", df.format(cookingTime / 20d))).noShadow().rightAligned().color(0xFF404040, 0xFFBBBBBB));
+        widgets.add(Widgets.createArrow(new Point(startPoint.x + 24, startPoint.y + 8)).animationDurationTicks(cookingTime));
+        widgets.add(Widgets.createSlot(new Point(startPoint.x + 1, startPoint.y + 1)).entries(display.getInputEntries().get(0)).markInput());
+        widgets.add(Widgets.createSlot(new Point(startPoint.x + 61, startPoint.y + 9)).entries(display.getResultingEntries().get(0)).disableBackground().markOutput());
+        return widgets;
+    }
+
+    @Override
+    public int getDisplayHeight() {
+        return 49;
+    }
+}
---
+++ src/main/java/com/github/platymemo/alaskanativecraft/compat/rei/DryingDisplay.java
@@ -0,0 +1,66 @@
+package com.github.platymemo.alaskanativecraft.compat.rei;
+
+import com.github.platymemo.alaskanativecraft.recipe.DryingRecipe;
+import me.shedaniel.rei.api.EntryStack;
+import me.shedaniel.rei.api.RecipeDisplay;
+import me.shedaniel.rei.plugin.DefaultPlugin;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.AbstractCookingRecipe;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.collection.DefaultedList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Environment(EnvType.CLIENT)
+public class DryingDisplay implements RecipeDisplay {
+    private List<List<EntryStack>> inputs;
+    private List<EntryStack> output;
+    private int dryTime;
+    private DryingRecipe display;
+
+    public DryingDisplay(DryingRecipe recipe) {
+        this(recipe.getPreviewInputs(), recipe.getOutput(), recipe.getCookTime());
+        this.display = recipe;
+    }
+
+    public DryingDisplay(DefaultedList<Ingredient> ingredients, ItemStack output, int dryTime) {
+        this.inputs = EntryStack.ofIngredients(ingredients);
+        this.output = Collections.singletonList(EntryStack.create(output));
+        this.dryTime = dryTime;
+    }
+
+    public double getDryTime() {
+        return dryTime;
+    }
+
+    @Override
+    public @NotNull Optional<Identifier> getRecipeLocation() {
+        return Optional.ofNullable(display).map(AbstractCookingRecipe::getId);
+    }
+
+    @Override
+    public @NotNull List<List<EntryStack>> getInputEntries() {
+        return inputs;
+    }
+
+    @Override
+    public @NotNull List<List<EntryStack>> getResultingEntries() {
+        return Collections.singletonList(output);
+    }
+
+    @Override
+    public @NotNull List<List<EntryStack>> getRequiredEntries() {
+        return inputs;
+    }
+
+    @Override
+    public @NotNull Identifier getRecipeCategory() {
+        return DefaultPlugin.CAMPFIRE;
+    }
+}
