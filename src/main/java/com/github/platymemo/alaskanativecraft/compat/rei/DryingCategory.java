3ba1d16818551d8c344bf1b5ef789f8b7b309211
shedaniel <daniel@shedaniel.me>
2021-06-03 15:39:50 +0800

    Update to REI 6

--- src/main/java/com/github/platymemo/alaskanativecraft/compat/rei/DryingCategory.java
+++ src/main/java/com/github/platymemo/alaskanativecraft/compat/rei/DryingCategory.java
@@ -1,36 +1,36 @@
-/*
 package com.github.platymemo.alaskanativecraft.compat.rei;
 
 import com.github.platymemo.alaskanativecraft.block.AlaskaBlocks;
 import com.google.common.collect.Lists;
 import me.shedaniel.math.Point;
 import me.shedaniel.math.Rectangle;
-import me.shedaniel.rei.api.EntryStack;
-import me.shedaniel.rei.api.RecipeCategory;
-import me.shedaniel.rei.api.widgets.Widgets;
-import me.shedaniel.rei.gui.widget.Widget;
-import net.minecraft.client.resource.language.I18n;
+import me.shedaniel.rei.api.client.gui.Renderer;
+import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import me.shedaniel.rei.api.client.gui.widgets.Widgets;
+import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.DecimalFormat;
 import java.util.List;
 
-public class DryingCategory implements RecipeCategory<DryingDisplay> {
+public class DryingCategory implements DisplayCategory<DryingDisplay> {
     @Override
-    public @NotNull Identifier getIdentifier() {
+    public CategoryIdentifier<? extends DryingDisplay> getCategoryIdentifier() {
         return AlaskaPlugin.DRYING_ID;
     }
 
     @Override
-    public @NotNull EntryStack getLogo() {
-        return EntryStack.create(AlaskaBlocks.DRYING_RACK);
+    public @NotNull Renderer getIcon() {
+        return EntryStacks.of(AlaskaBlocks.DRYING_RACK);
     }
 
     @Override
-    public @NotNull String getCategoryName() {
-        return I18n.translate("category.rei.alaskanativecraft.drying");
+    public Text getTitle() {
+        return new TranslatableText("category.rei.alaskanativecraft.drying");
     }
 
     @Override
@@ -45,7 +45,7 @@ public class DryingCategory implements RecipeCategory<DryingDisplay> {
                 new TranslatableText("category.rei.campfire.time", df.format(cookingTime / 20d))).noShadow().rightAligned().color(0xFF404040, 0xFFBBBBBB));
         widgets.add(Widgets.createArrow(new Point(startPoint.x + 24, startPoint.y + 8)).animationDurationTicks(cookingTime));
         widgets.add(Widgets.createSlot(new Point(startPoint.x + 1, startPoint.y + 9)).entries(display.getInputEntries().get(0)).markInput());
-        widgets.add(Widgets.createSlot(new Point(startPoint.x + 61, startPoint.y + 9)).entries(display.getResultingEntries().get(0)).disableBackground().markOutput());
+        widgets.add(Widgets.createSlot(new Point(startPoint.x + 61, startPoint.y + 9)).entries(display.getOutputEntries().get(0)).disableBackground().markOutput());
         return widgets;
     }
 
@@ -54,4 +54,3 @@ public class DryingCategory implements RecipeCategory<DryingDisplay> {
         return 49;
     }
 }
-*/
