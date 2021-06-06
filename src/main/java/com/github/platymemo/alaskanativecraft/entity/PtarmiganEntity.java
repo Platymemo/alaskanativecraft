dcdae1ea9d2d5836afc5f55c38a149e0dd408a44
Platymemo <edwardhazelton+github@gmail.com>
2021-06-05 19:20:43 -0800

    Removed Dumbassery (clinit circular reasoning)

--- src/main/java/com/github/platymemo/alaskanativecraft/entity/PtarmiganEntity.java
+++ src/main/java/com/github/platymemo/alaskanativecraft/entity/PtarmiganEntity.java
@@ -1,5 +1,7 @@
 package com.github.platymemo.alaskanativecraft.entity;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
@@ -14,15 +16,25 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+
+import java.util.Random;
 
 public class PtarmiganEntity extends ParrotEntity {
     private static final TrackedData<Integer> TYPE;
 
     public PtarmiganEntity(EntityType<? extends ParrotEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    public static boolean isValidSpawn(EntityType<PtarmiganEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
+        BlockState blockState = world.getBlockState(pos.down());
+        return (blockState.isIn(BlockTags.LEAVES) || blockState.isOf(Blocks.GRASS_BLOCK) || blockState.isIn(BlockTags.LOGS) || blockState.isOf(Blocks.AIR)) && world.getBaseLightLevel(pos, 0) > 8;
     }
 
     public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, EntityData entityData, CompoundTag entityTag) {
