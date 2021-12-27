a254966df2bd714edc3b145acb4621d76c2ca8bf
Platymemo <edwardhazelton+github@gmail.com>
2021-12-27 04:42:11 -0900

    fix seal baby model

--- src/main/java/com/github/platymemo/alaskanativecraft/client/model/entity/SealEntityModel.java
+++ src/main/java/com/github/platymemo/alaskanativecraft/client/model/entity/SealEntityModel.java
@@ -5,7 +5,12 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.model.*;
+import net.minecraft.client.model.ModelData;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.model.ModelPartBuilder;
+import net.minecraft.client.model.ModelPartData;
+import net.minecraft.client.model.ModelTransform;
+import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.render.entity.model.EntityModelPartNames;
 import net.minecraft.client.render.entity.model.QuadrupedEntityModel;
 import net.minecraft.util.math.MathHelper;
@@ -19,52 +24,48 @@ public class SealEntityModel<T extends SealEntity> extends QuadrupedEntityModel<
     protected final ModelPart tail;
 
     public SealEntityModel(ModelPart root) {
-        super(root, true, 24.0F, 0.0F, 2.0F, 1.5F, 24);
+        super(root, true, 9.0F, 1.0F, 2.0F, 1.5F, 12);
         stomach = root.getChild(STOMACH);
         tail = root.getChild(TAIL);
     }
 
     public static @NotNull TexturedModelData getTexturedModelData() {
-        return getTexturedModelData(new Dilation(0));
-    }
-
-    public static @NotNull TexturedModelData getTexturedModelData(Dilation dilation) {
         ModelData modelData = new ModelData();
         ModelPartData root = modelData.getRoot();
         root.addChild(EntityModelPartNames.RIGHT_FRONT_LEG,
-                ModelPartBuilder.create().uv(38, 6).cuboid(-5.0F, -1.0F, -1.0F, 5.0F, 1.0F, 3.0F, dilation),
+                ModelPartBuilder.create().uv(38, 6).cuboid(-5.0F, -1.0F, -1.0F, 5.0F, 1.0F, 3.0F),
                 ModelTransform.of(-6.0F, 24.0F, -6.0F, 0.0F, -1.0F, 0.0F)
         );
         root.addChild(EntityModelPartNames.LEFT_FRONT_LEG,
-                ModelPartBuilder.create().uv(32, 27).cuboid(0.0F, -1.0F, -1.0F, 5.0F, 1.0F, 3.0F, dilation),
+                ModelPartBuilder.create().uv(32, 27).cuboid(0.0F, -1.0F, -1.0F, 5.0F, 1.0F, 3.0F),
                 ModelTransform.of(5.0F, 24.0F, -6.0F, 0.0F, 1.0F, 0.0F)
         );
         root.addChild(EntityModelPartNames.RIGHT_HIND_LEG,
-                ModelPartBuilder.create().uv(30, 0).cuboid(-2.0F, 0.0F, -1.0F, 3.0F, 1.0F, 5.0F, dilation),
+                ModelPartBuilder.create().uv(30, 0).cuboid(-2.0F, 0.0F, -1.0F, 3.0F, 1.0F, 5.0F),
                 ModelTransform.pivot(-3.0F, 23.0F, 11.0F)
         );
         root.addChild(EntityModelPartNames.LEFT_HIND_LEG,
-                ModelPartBuilder.create().uv(13, 38).cuboid(-1.0F, 0.0F, -1.1F, 3.0F, 1.0F, 5.0F, dilation),
+                ModelPartBuilder.create().uv(13, 38).cuboid(-1.0F, 0.0F, -1.1F, 3.0F, 1.0F, 5.0F),
                 ModelTransform.pivot(2.0F, 23.0F, 11.1F)
         );
 
         root.addChild(EntityModelPartNames.HEAD,
                 ModelPartBuilder.create()
-                        .uv(29, 31).cuboid(EntityModelPartNames.NECK, -4.5F, -5.0F, -3.0F, 9.0F, 9.0F, 3.0F, dilation)
-                        .uv(32, 18).cuboid(-3.5F, -4.0F, -5.0F, 7.0F, 7.0F, 2.0F, dilation)
-                        .uv(38, 10).cuboid(EntityModelPartNames.NOSE, -2.5F, -1.0F, -7.0F, 5.0F, 3.0F, 2.0F, dilation),
+                        .uv(29, 31).cuboid(EntityModelPartNames.NECK, -4.5F, -5.0F, -3.0F, 9.0F, 9.0F, 3.0F)
+                        .uv(32, 18).cuboid(-3.5F, -4.0F, -5.0F, 7.0F, 7.0F, 2.0F)
+                        .uv(38, 10).cuboid(EntityModelPartNames.NOSE, -2.5F, -1.0F, -7.0F, 5.0F, 3.0F, 2.0F),
                 ModelTransform.pivot(-0.5F, 19.0F, -8.0F)
         );
         root.addChild(EntityModelPartNames.BODY,
-                ModelPartBuilder.create().uv(0, 0).cuboid(-5.5F, -5.0F, -4.0F, 11.0F, 10.0F, 8.0F, dilation),
+                ModelPartBuilder.create().uv(0, 0).cuboid(-5.5F, -5.0F, -4.0F, 11.0F, 10.0F, 8.0F),
                 ModelTransform.pivot(-0.5F, 19.0F, -4.0F)
         );
         root.addChild(STOMACH,
-                ModelPartBuilder.create().uv(0, 18).cuboid(-4.5F, -4.5F, -3.5F, 9.0F, 9.0F, 7.0F, dilation),
+                ModelPartBuilder.create().uv(0, 18).cuboid(-4.5F, -4.5F, -3.5F, 9.0F, 9.0F, 7.0F),
                 ModelTransform.pivot(-0.5F, 19.5F, 3.5F)
         );
         root.addChild(TAIL,
-                ModelPartBuilder.create().uv(0, 34).cuboid(-2.5F, -2.5F, -2.0F, 5.0F, 5.0F, 4.0F, dilation),
+                ModelPartBuilder.create().uv(0, 34).cuboid(-2.5F, -2.5F, -2.0F, 5.0F, 5.0F, 4.0F),
                 ModelTransform.pivot(-0.5F, 21.0F, 9.0F)
         );
         return TexturedModelData.of(modelData, 64, 64);
