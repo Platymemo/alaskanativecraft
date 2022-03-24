a0000a1d0652773c3445ef619033a426bf9e680e
Sunconure11 <eyespot7b@aol.com>
2022-03-24 13:20:41 -0400

    Partially fixed camps

--- src/main/java/com/github/platymemo/alaskanativecraft/worldgen/structure/FishCampStructure.java
+++ src/main/java/com/github/platymemo/alaskanativecraft/worldgen/structure/FishCampStructure.java
@@ -2,20 +2,21 @@ package com.github.platymemo.alaskanativecraft.worldgen.structure;
 
 import com.github.platymemo.alaskanativecraft.AlaskaNativeCraft;
 import com.mojang.serialization.Codec;
-import net.minecraft.structure.PoolStructurePiece;
-import net.minecraft.structure.PostPlacementProcessor;
-import net.minecraft.structure.StructureGeneratorFactory;
-import net.minecraft.structure.StructurePiecesGenerator;
+import net.minecraft.structure.*;
 import net.minecraft.structure.pool.StructurePoolBasedGenerator;
+import net.minecraft.tag.BiomeTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.StructureFeature;
 import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
+import net.minecraft.world.gen.random.AtomicSimpleRandom;
+import net.minecraft.world.gen.random.ChunkRandom;
 
 import java.util.Optional;
 import java.util.Set;
@@ -32,47 +33,39 @@ public class FishCampStructure extends StructureFeature<StructurePoolFeatureConf
      * b) A river or ocean is within 32 blocks
      */
     private static boolean isFeatureChunk(StructureGeneratorFactory.Context<StructurePoolFeatureConfig> context) {
-        if (isVillageNearby(context.chunkGenerator(), context.seed(), context.chunkPos())) return false;
-
+        ChunkPos chunkpos = context.chunkPos();
         BlockPos spawnXZPosition = context.chunkPos().getCenterAtY(0);
         int landHeight = context.chunkGenerator().getHeightInGround(spawnXZPosition.getX(), spawnXZPosition.getZ(), Heightmap.Type.WORLD_SURFACE_WG, context.world());
 
-        Set<Biome> biomes = context.chunkGenerator().getBiomeSource().getBiomesInArea(spawnXZPosition.getX(), landHeight, spawnXZPosition.getZ(), 32, context.chunkGenerator().getMultiNoiseSampler());
-        return biomes.stream().anyMatch(biome -> biome.getCategory() == Biome.Category.RIVER || biome.getCategory() == Biome.Category.OCEAN);
+        Set<RegistryEntry<Biome>> biomes = context.chunkGenerator().getBiomeSource().getBiomesInArea(spawnXZPosition.getX(), landHeight, spawnXZPosition.getZ(), 32, context.chunkGenerator().getMultiNoiseSampler());
+        return biomes.stream().anyMatch(biome -> biome.isIn(BiomeTags.IS_RIVER) || biome.isIn(BiomeTags.IS_OCEAN)) && !context.chunkGenerator().method_41053(StructureSetKeys.VILLAGES, context.seed(), chunkpos.x, chunkpos.z, 10);
     }
 
     /**
      * Checks for a village within 10 chunks
      * identical to {@link net.minecraft.world.gen.feature.PillagerOutpostFeature::isVillageNearby}
      */
-    private static boolean isVillageNearby(ChunkGenerator chunkGenerator, long seed, ChunkPos chunkPos) {
-        StructureConfig structureConfig = chunkGenerator.getStructuresConfig().getForType(StructureFeature.VILLAGE);
-        if (structureConfig != null) {
-            for (int k = chunkPos.x - 10; k <= chunkPos.x + 10; ++k) {
-                for (int l = chunkPos.z - 10; l <= chunkPos.z + 10; ++l) {
-                    ChunkPos chunkPos2 = StructureFeature.VILLAGE.getStartChunk(structureConfig, seed, k, l);
-                    if (k == chunkPos2.x && l == chunkPos2.z) {
-                        return true;
-                    }
-                }
-            }
+    private static boolean canGenerate(StructureGeneratorFactory.Context<StructurePoolFeatureConfig> context) {
+        ChunkPos chunkPos = context.chunkPos();
+        int i = chunkPos.x >> 4;
+        int j = chunkPos.z >> 4;
+        ChunkRandom chunkRandom = new ChunkRandom(new AtomicSimpleRandom(0L));
+        chunkRandom.setSeed((long)(i ^ j << 4) ^ context.seed());
+        chunkRandom.nextInt();
+        if (chunkRandom.nextInt(5) != 0) {
+            return false;
+        } else {
+            return !context.chunkGenerator().method_41053(StructureSetKeys.VILLAGES, context.seed(), chunkPos.x, chunkPos.z, 10);
         }
-        return false;
     }
 
     public static Optional<StructurePiecesGenerator<StructurePoolFeatureConfig>> createPiecesGenerator(StructureGeneratorFactory.Context<StructurePoolFeatureConfig> context) {
-
-        // Check if the spot is valid for our structure.
         if (!FishCampStructure.isFeatureChunk(context)) {
             return Optional.empty();
         }
 
-        StructurePoolFeatureConfig newConfig = new StructurePoolFeatureConfig(
-                () -> context.registryManager()
-                        .get(Registry.STRUCTURE_POOL_KEY)
-                        .get(new Identifier(AlaskaNativeCraft.MOD_ID, "fish_camp/base")),
-                3
-        );
+        StructurePoolFeatureConfig newConfig = new StructurePoolFeatureConfig(() -> context.registryManager().get(Registry.STRUCTURE_POOL_KEY)
+                .get(new Identifier(AlaskaNativeCraft.MOD_ID, "fish_camp/base")), 3);
 
         StructureGeneratorFactory.Context<StructurePoolFeatureConfig> newContext = new StructureGeneratorFactory.Context<>(
                 context.chunkGenerator(),
@@ -83,10 +76,8 @@ public class FishCampStructure extends StructureFeature<StructurePoolFeatureConf
                 context.world(),
                 context.validBiome(),
                 context.structureManager(),
-                context.registryManager()
-        );
+                context.registryManager());
         BlockPos blockpos = context.chunkPos().getCenterAtY(0);
-
         return StructurePoolBasedGenerator.generate(newContext, PoolStructurePiece::new, blockpos, false, true);
     }
 }
