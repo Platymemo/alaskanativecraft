f744900aed9752ed0cd62e110db58d89d228f943
Sunconure11 <eyespot7b@aol.com>
2022-09-22 16:52:23 -0400

    Hmm

--- src/main/java/com/github/platymemo/alaskanativecraft/entity/AlaskaEntities.java
+++ src/main/java/com/github/platymemo/alaskanativecraft/entity/AlaskaEntities.java
@@ -7,7 +7,6 @@ import com.github.platymemo.alaskanativecraft.item.HarpoonItem;
 import com.github.platymemo.alaskanativecraft.tags.AlaskaTags;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.fabricmc.fabric.mixin.object.builder.SpawnRestrictionAccessor;
 import net.minecraft.entity.*;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.util.Identifier;
@@ -45,9 +44,9 @@ public class AlaskaEntities {
     }
 
     private static void initSpawnRestrictions() {
-        SpawnRestriction.canSpawn(HARP_SEAL, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, SealEntity::canSpawn);
-        SpawnRestriction.canSpawn(MOOSE, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
-        SpawnRestriction.canSpawn(PTARMIGAN, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING, PtarmiganEntity::isValidSpawn);
+        SpawnRestriction.register(HARP_SEAL, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, SealEntity::canSpawn);
+        SpawnRestriction.register(MOOSE, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
+        SpawnRestriction.register(PTARMIGAN, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING, PtarmiganEntity::isValidSpawn);
     }
 
     private static void initAttributes() {
